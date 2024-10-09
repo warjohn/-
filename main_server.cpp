33516916e94d9673276c911d7c7c5ad3bce49efd
menar <johnvoronina@gmail.com>
2024-10-09 22:29:36 +0700

    test_0.2

--- main_server.cpp
+++ main_server.cpp
@@ -1,58 +1,104 @@
 #include <iostream>
 #include <fstream>
-#include "sys/socket.h"
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
 using namespace std;
 
+void initializeSSL() {
+    SSL_load_error_strings();   
+    OpenSSL_add_ssl_algorithms();
+}
+
+void cleanupSSL() {
+    EVP_cleanup();
+}
+
 int main() {
+    initializeSSL();
 
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    
-    /*
-    socketfd: Это файловый дескриптор для сокета.
-    AF_INET: Указывает семейство протоколов IPv4.
-    SOCK_STREAM: Определяет тип сокета TCP.
-    */
-
-    if (serverSocket == -1) {
-        cerr << "Не удалось создать сокет." << endl;
+    SSL_CTX* ctx = SSL_CTX_new(TLS_server_method());
+    if (!ctx) {
+        cerr << "Не удалось создать SSL_CTX." << endl;
         return 1;
     }
 
-    sockaddr_in serverAddress; //Это тип данных, используемый для хранения адреса сокета.
-    serverAddress.sin_family = AF_INET;  // Указываем семейство адресов (IPv4)
-    serverAddress.sin_port = htons(8080);  /* Указываем порт (с преобразованием в сетевой порядок байт) Эта функция используется для преобразования unsigned int из машинного порядка байтов в сетевой порядок.*/
-    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);  //  Это используется, когда мы не хотим привязывать сокет к определенному IP-адресу, а вместо этого хотим прослушивать все доступные IP-адреса.
-    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)); //Затем мы связываем сокет с помощью вызова bind(),
-    listen(serverSocket, 5); //Затем мы сообщаем приложению прослушивать сокет, на который ссылается serverSocket
+    // Загрузите сертификат и ключ
+    if (SSL_CTX_use_certificate_file(ctx, "server.crt", SSL_FILETYPE_PEM) <= 0) {
+        cerr << "Не удалось загрузить сертификат." << endl;
+        SSL_CTX_free(ctx);
+        return 1;
+    }
 
-    int clientSocket = accept(serverSocket, nullptr, nullptr); //Вызов accept() используется для принятия запроса на соединение, полученного на сокете, который прослушивало приложение
+    if (SSL_CTX_use_PrivateKey_file(ctx, "server.key", SSL_FILETYPE_PEM) <= 0) {
+        cerr << "Не удалось загрузить закрытый ключ." << endl;
+        SSL_CTX_free(ctx);
+        return 1;
+    }
+
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        cerr << "Не удалось создать сокет." << endl;
+        SSL_CTX_free(ctx);
+        return 1;
+    }
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    listen(serverSocket, 5);
+
+    int clientSocket = accept(serverSocket, nullptr, nullptr);
+    if (clientSocket < 0) {
+        cerr << "Не удалось принять соединение." << endl;
+        close(serverSocket);
+        SSL_CTX_free(ctx);
+        return 1;
+    }
+
+    SSL* ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, clientSocket);
+    
+    if (SSL_accept(ssl) <= 0) {
+        cerr << "Ошибка SSL_accept." << endl;
+        SSL_free(ssl);
+        close(clientSocket);
+        close(serverSocket);
+        SSL_CTX_free(ctx);
+        return 1;
+    }
 
     ofstream file("received_file.txt", ios::binary);
     if (!file.is_open()) {
         cerr << "Не удалось создать файл." << endl;
+        SSL_free(ssl);
+        close(clientSocket);
+        close(serverSocket);
+        SSL_CTX_free(ctx);
+        return 1;
     }
 
     char buffer[1024];
     int bytesReceived;
-    while ((bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
+    while ((bytesReceived = SSL_read(ssl, buffer, sizeof(buffer))) > 0) {
         cout << "\tДанные передачи: " << buffer << endl;
-        file.write(buffer, bytesReceived);  // Записываем полученные данные в файл
+        file.write(buffer, bytesReceived);
     }
 
-
-    //char buffer[1024] = {0};
-    //recv(clientSocket, buffer, sizeof(buffer), 0);
-    //cout << "Message from client: " << buffer << endl;
-    /*
-    Затем мы начинаем получать данные от клиента. 
-    Мы можем указать необходимый размер буфера, чтобы в нем было достаточно места для приема данных, отправляемых клиентом
-    */
     file.close();
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
+    close(clientSocket);
     close(serverSocket);
+    SSL_CTX_free(ctx);
+    cleanupSSL();
 
     cout << "Команда завершила выполнение!" << endl;
+    return 0;
 }
